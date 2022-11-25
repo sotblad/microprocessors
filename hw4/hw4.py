52fb362aebe39dac31075474520e95b36ade0cf4
sotblad <sotiris2009@hotmail.com>
2022-11-25 15:20:47 +0200

    4.1 complete

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,3 +1,8 @@
+import random
+import copy
+import matplotlib.pyplot as plt
+
+
 def spAND(inputs):
     s = inputs[0]
     for i in range(1, len(inputs)):
@@ -53,6 +58,34 @@ class Entity:
 
     def __str__(self):
         return "{type=" + str(self.type) + ", inputs=" + str(self.inputs) + ", output=" + str(self.output) + "}"
+
+class Individual:
+    def __init__(self, workload):
+        self.workload = workload
+        self.score = 0
+
+    def setScore(self, score):
+        self.score = score
+
+    def __str__(self):
+        return "{workload=" + str(self.workload) + ", score=" + str(self.score) + "}"
+
+def randomWorkload(inputs, L):
+    workload = []
+    for i in range(0, L):
+        tmp = []
+        for j in range(0, len(inputs)):
+            tmp.append(random.randint(0, 1))
+        workload.append(tmp)
+    return workload
+
+
+def countSwitches(SignalsBefore, SignalsAfter, tlpinputs):
+    switchesnumber = 0
+    for j in list(set(SignalsAfter.keys())-set(tlpinputs)):
+        if SignalsBefore[j] != SignalsAfter[j]:
+            switchesnumber += 1
+    return switchesnumber
 
 
 def process(element, SignalsTable):
@@ -115,13 +148,11 @@ def readCircuit():
     return [[lines[0]]+flag[0], inputs]
 
 
-def main():
-    sortedCircuit = readCircuit()
-
+def preprocess(circuit):
     ElementsTable = []
     ElementTypes = ["AND", "OR", "XOR", "XNOR", "NAND", "NOR", "NOT"]
 
-    for i in sortedCircuit[0][1:]:
+    for i in circuit[0][1:]:
         splitLine = i.split()
         type = splitLine[0]
         output = splitLine[1]
@@ -135,11 +166,51 @@ def main():
         for j in i.inputs:
             SignalsTable[j] = 0
         SignalsTable[i.output] = 0
+    return [ElementsTable, SignalsTable]
 
-    for i in ElementsTable:
-        SignalsTable = process(i, SignalsTable)
 
-    print(SignalsTable)
+def setInputs(inputs, inputKeys, signalTable):
+    for i in range(0, len(inputKeys)):
+        signalTable[inputKeys[i]] = inputs[i]
+    return signalTable
+
+
+def calculateScores(IndividualsTable, ElementsTable, sortedCircuit, SignalsTable):
+    for i in range(0, len(IndividualsTable)):
+        SignalsTable1 = setInputs(IndividualsTable[i].workload[0], sortedCircuit[1], SignalsTable)
+
+        for j in ElementsTable:
+            SignalsTable1 = process(j, SignalsTable1)
+        signalsBefore = copy.deepcopy(SignalsTable1)
+
+        SignalsTable2 = setInputs(IndividualsTable[i].workload[1], sortedCircuit[1], SignalsTable)
+
+        for j in ElementsTable:
+            SignalsTable2 = process(j, SignalsTable2)
+        signalsAfter = copy.deepcopy(SignalsTable2)
+
+        switchesCounter = countSwitches(signalsBefore, signalsAfter, sortedCircuit[1])
+        IndividualsTable[i].score = switchesCounter
+
+def main():
+    Individuals = 2000
+    sortedCircuit = readCircuit()
+    pp = preprocess(sortedCircuit)
+    ElementsTable = pp[0]
+    SignalsTable = pp[1]
+   # print(SignalsTable)
+
+    IndividualsTable = []
+    for i in range(0, Individuals):
+        randWorkload = randomWorkload(sortedCircuit[1], 2)
+        IndividualsTable.append(Individual(randWorkload))
+
+    calculateScores(IndividualsTable, ElementsTable, sortedCircuit, SignalsTable)
+
+    x = list(range(0, len(IndividualsTable)))
+    y = [i.score for i in IndividualsTable]
+    plt.plot(x, y)
+    plt.show()
 
 
 if __name__ == "__main__":
